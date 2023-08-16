e926cb26d2ef8b358a9160cd0d70a58655738867
duardoqueiroz <eduardoldq1@gmail.com>
2023-08-16 17:14:50 -0300

    chore: create main pages files

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      {/* IMAGE */}
+      {/* TITLE */}
+      {/* TEXT */}
+      {/* BUTTON TO REDIRECT TO SIGN-UP */}
+    </View>
+  );
+};
+
+export default Home;
