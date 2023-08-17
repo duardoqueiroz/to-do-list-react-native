2020adb194ae51b0b8334b9666bead1e5a51f7e2
duardoqueiroz <eduardoldq1@gmail.com>
2023-08-17 17:57:46 -0300

    chore: setup navigation

--- App.tsx
+++ App.tsx
@@ -2,7 +2,27 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/pages/Home/index";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SignUp from "./src/pages/Auth/SignUp";
+import SignIn from "./src/pages/Auth/SignIn";
+import Tasks from "./src/pages/Tasks";
 
 export default function App() {
-  return <Home></Home>;
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="Tasks" component={Tasks} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
