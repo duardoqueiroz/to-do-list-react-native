17584e898eb660a9be4cfbb9662c75b6f08ced02
duardoqueiroz <eduardoldq1@gmail.com>
2023-08-17 17:59:10 -0300

    chore: setup firebase app

---
+++ src/services/firebase.ts
@@ -0,0 +1,15 @@
+import { FirebaseOptions, initializeApp } from "firebase/app";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKER,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
