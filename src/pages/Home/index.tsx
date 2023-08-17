a17bb95069bc61aeefdb62322e41c712ef090825
duardoqueiroz <eduardoldq1@gmail.com>
2023-08-17 17:58:17 -0300

    feat: home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,57 @@
-import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import styles from "./styles";
+import Button from "../../components/Button";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "../../services/firebase";
+interface HomeProps {
+  navigation: any;
+}
 
-const Home = () => {
+const Home = ({ navigation }: HomeProps) => {
   return (
     <View style={styles.container}>
+      <Image
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: 254,
+          height: 228,
+        }}
+        source={require("../../../assets/shape.png")}
+      ></Image>
       {/* IMAGE */}
-      {/* TITLE */}
-      {/* TEXT */}
-      {/* BUTTON TO REDIRECT TO SIGN-UP */}
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../../../assets/home-undraw.png")}
+        ></Image>
+      </View>
+      {/* TITLE AND TEXT */}
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Get things done with TODO</Text>
+        <Text style={styles.paragraph}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere
+          gravida purus id eu condimentum est diam quam. Condimentum blandit
+          diam.
+        </Text>
+      </View>
+      {/* BUTTON */}
+      <View style={styles.buttonContainer}>
+        <Button
+          text="Get Started"
+          onPress={() => {
+            const auth = getAuth(app);
+            onAuthStateChanged(auth, (user) => {
+              if (user) {
+                navigation.navigate("Tasks", { user });
+              } else {
+                navigation.navigate("SignUp");
+              }
+            });
+          }}
+        ></Button>
+      </View>
     </View>
   );
 };
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -3,9 +3,36 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "##EEEEEE",
+  },
+  imageContainer: {
+    marginHorizontal: "auto",
+    paddingTop: 164,
+    paddingBottom: 40,
+  },
+  image: {
+    width: 187.16,
+    height: 170,
+  },
+  textContainer: {
+    paddingVertical: 40,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "700",
+    lineHeight: 33,
+    textAlign: "center",
+  },
+  paragraph: {
+    paddingHorizontal: 30,
+    fontWeight: "600",
+    fontSize: 15,
+    lineHeight: 22.5,
+    textAlign: "center",
+  },
+  buttonContainer: {
+    paddingTop: 80,
   },
 });
 
